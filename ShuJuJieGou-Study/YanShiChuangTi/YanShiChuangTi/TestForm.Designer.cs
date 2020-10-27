ab850205c7d2c5b7fb6bc8f500dcd94f9336666d
zzq361267211 <66397159+zzq361267211@users.noreply.github.com>
2020-10-27 20:09:31 +0800

    清理多余代码

--- ShuJuJieGou-Study/YanShiChuangTi/YanShiChuangTi/TestForm.Designer.cs
+++ ShuJuJieGou-Study/YanShiChuangTi/YanShiChuangTi/TestForm.Designer.cs
@@ -30,11 +30,16 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.btn_LinkedListStack = new System.Windows.Forms.Button();
+            this.btn_Array = new System.Windows.Forms.Button();
+            this.btn_ArrayQuene = new System.Windows.Forms.Button();
+            this.btn_ArrayStack = new System.Windows.Forms.Button();
+            this.btn_KuoHaoPiPei = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(127, 174);
+            this.button1.Location = new System.Drawing.Point(53, 237);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(115, 50);
             this.button1.TabIndex = 0;
@@ -44,7 +49,7 @@
             // 
             // btn_LinkedListStack
             // 
-            this.btn_LinkedListStack.Location = new System.Drawing.Point(127, 255);
+            this.btn_LinkedListStack.Location = new System.Drawing.Point(53, 318);
             this.btn_LinkedListStack.Name = "btn_LinkedListStack";
             this.btn_LinkedListStack.Size = new System.Drawing.Size(115, 46);
             this.btn_LinkedListStack.TabIndex = 1;
@@ -52,16 +57,69 @@
             this.btn_LinkedListStack.UseVisualStyleBackColor = true;
             this.btn_LinkedListStack.Click += new System.EventHandler(this.btn_LinkedListStack_Click);
             // 
+            // btn_Array
+            // 
+            this.btn_Array.Location = new System.Drawing.Point(53, 157);
+            this.btn_Array.Name = "btn_Array";
+            this.btn_Array.Size = new System.Drawing.Size(115, 49);
+            this.btn_Array.TabIndex = 2;
+            this.btn_Array.Text = "动态数组示例";
+            this.btn_Array.UseVisualStyleBackColor = true;
+            this.btn_Array.Click += new System.EventHandler(this.btn_Array_Click);
+            // 
+            // btn_ArrayQuene
+            // 
+            this.btn_ArrayQuene.Location = new System.Drawing.Point(53, 94);
+            this.btn_ArrayQuene.Name = "btn_ArrayQuene";
+            this.btn_ArrayQuene.Size = new System.Drawing.Size(115, 42);
+            this.btn_ArrayQuene.TabIndex = 3;
+            this.btn_ArrayQuene.Text = "数组队列";
+            this.btn_ArrayQuene.UseVisualStyleBackColor = true;
+            this.btn_ArrayQuene.Click += new System.EventHandler(this.btn_ArrayQuene_Click);
+            // 
+            // btn_ArrayStack
+            // 
+            this.btn_ArrayStack.Location = new System.Drawing.Point(307, 66);
+            this.btn_ArrayStack.Name = "btn_ArrayStack";
+            this.btn_ArrayStack.Size = new System.Drawing.Size(96, 48);
+            this.btn_ArrayStack.TabIndex = 4;
+            this.btn_ArrayStack.Text = "数组栈";
+            this.btn_ArrayStack.UseVisualStyleBackColor = true;
+            this.btn_ArrayStack.Click += new System.EventHandler(this.btn_ArrayStack_Click);
+            // 
+            // btn_KuoHaoPiPei
+            // 
+            this.btn_KuoHaoPiPei.Location = new System.Drawing.Point(307, 158);
+            this.btn_KuoHaoPiPei.Name = "btn_KuoHaoPiPei";
+            this.btn_KuoHaoPiPei.Size = new System.Drawing.Size(139, 48);
+            this.btn_KuoHaoPiPei.TabIndex = 5;
+            this.btn_KuoHaoPiPei.Text = "括号匹配-力扣20";
+            this.btn_KuoHaoPiPei.UseVisualStyleBackColor = true;
+            this.btn_KuoHaoPiPei.Click += new System.EventHandler(this.btn_KuoHaoPiPei_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(443, 175);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 6;
+            // 
             // TestForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_KuoHaoPiPei);
+            this.Controls.Add(this.btn_ArrayStack);
+            this.Controls.Add(this.btn_ArrayQuene);
+            this.Controls.Add(this.btn_Array);
             this.Controls.Add(this.btn_LinkedListStack);
             this.Controls.Add(this.button1);
             this.Name = "TestForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +127,11 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btn_LinkedListStack;
+        private System.Windows.Forms.Button btn_Array;
+        private System.Windows.Forms.Button btn_ArrayQuene;
+        private System.Windows.Forms.Button btn_ArrayStack;
+        private System.Windows.Forms.Button btn_KuoHaoPiPei;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- ShuJuJieGou-Study/YanShiChuangTi/YanShiChuangTi/TestForm.cs
+++ ShuJuJieGou-Study/YanShiChuangTi/YanShiChuangTi/TestForm.cs
@@ -46,5 +46,123 @@ namespace ShuJuJieGouStudy
             linkedListStack.Pop();
             Console.WriteLine(linkedListStack.ToString());
         }
+
+        private void btn_Array_Click(object sender, EventArgs e)
+        {
+            Array<String> arr = new Array<String>(5);
+            for (int i = 0; i < 10; i++)
+            {
+                arr.AddLast(i.ToString() + "个人");
+                Console.WriteLine(arr.ToString());
+            }
+
+
+            arr.Add(1, 100.ToString() + "个人");
+            Console.WriteLine(arr.ToString());
+
+
+
+            for (int i = 0; i < 11; i++)
+            {
+                arr.RemoveLast();
+                Console.WriteLine(arr.ToString());
+            }
+        }
+
+        private void btn_ArrayQuene_Click(object sender, EventArgs e)
+        {
+            ArrayQuene<int> arrayQuene = new ArrayQuene<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                arrayQuene.Enquene(i + 1);
+                Console.WriteLine(arrayQuene.ToString());
+                if (i % 3 == 2)
+                {
+                    arrayQuene.Dequene();
+                    Console.WriteLine(arrayQuene.ToString());
+                }
+            }
+
+        }
+
+        private void btn_ArrayStack_Click(object sender, EventArgs e)
+        {
+            ArryStack<int> arryStack = new ArryStack<int>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                arryStack.Push(i);
+                Console.WriteLine(arryStack.ToString());
+            }
+
+            arryStack.Pop();
+            Console.WriteLine(arryStack.ToString());
+        }
+        
+
+        private void btn_KuoHaoPiPei_Click(object sender, EventArgs e)
+        {
+            bool b = IsVailed(textBox1.Text);
+            if (!b)
+            {
+                MessageBox.Show("不匹配");
+            }
+        }
+
+        private bool IsVailed(string str)
+        {
+            ArryStack<char> arryStack = new ArryStack<char>();
+            for (int i = 0; i < str.Length; i++)
+            {
+
+                if (str[i] == '{' || str[i] == '[' || str[i] == '(')
+                {
+                    arryStack.Push(str[i]);
+                }
+                else if (str[i] == '}' || str[i] == ']' || str[i] == ')')
+                {
+                    if (arryStack.IsEmpty() == true)
+                    {
+                        return false;
+                    }
+                    else if (arryStack.Peek() == '{')
+                    {
+                        if (str[i] == '}')
+                        {
+                            arryStack.Pop();
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else if (arryStack.Peek() == '[')
+                    {
+                        if (str[i] == ']')
+                        {
+                            arryStack.Pop();
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else if (arryStack.Peek() == '(')
+                    {
+                        if (str[i] == ')')
+                        {
+                            arryStack.Pop();
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            return arryStack.IsEmpty();
+
+        }
     }
 }
